875bc192ad587e81be469f15c84855833f9f12f4
Masafumi Horimoto on badman <horimoto.masafumi.470@s.kyushu-u.ac.jp>
2023-08-31 18:17:23 +0900

    途中

--- e10s.py
+++ e10s.py
@@ -1,7 +1,12 @@
 #! /usr/bin/env python3
+#coding: utf-8
+#
 import socket
+import netifaces
+import os
 
-def main():
+
+def rx_test():
     host = '127.0.0.1'
     port = 12345
 
@@ -14,5 +19,12 @@ def main():
             data, addr = sock.recvfrom(1024)
             print(f"Received data: {data} from {addr}")
 
+
+def init():
+    try:
+        HOST = netifaces.ifaddresses('eth0')[netifaces.AF_INET][0]['addr']
+        break
+    except KeyError:
+        
 if __name__ == "__main__":
-    main()
+    rx_test()
