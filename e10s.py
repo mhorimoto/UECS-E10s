1d30e086dffd2cb8768f081f5a2d5fc29a0ad1fd
Masafumi Horimoto on atom3 <horimoto@atom3.holly-linux.com>
2023-09-01 14:55:34 +0900

    とりあえず出来た

--- e10s.py
+++ e10s.py
@@ -20,21 +20,32 @@ def rx_test():
             print(f"Received data: {data} from {addr}")
 
 
-def init():
+def e10s_send(m,u,t,a):
     for i in netifaces.interfaces():
         if i=="lo":
             continue
         try:
             HOST = netifaces.ifaddresses(i)[netifaces.AF_INET][0]['addr']
+            print(HOST)
             break
         except:
             print(f"no {i} interface")
             continue
-    ADDRESS = netifaces.ifaddresses('eth0')[netifaces.AF_INET][0]['broadcast']
+    ADDRESS = netifaces.ifaddresses(i)[netifaces.AF_INET][0]['broadcast']
     PORT = 16520
-    with socket.socket(socket.AF_INET, socket.SOCK_DGRAM) as sock:
-        sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-        sock.bind((HOST, PORT))
+    sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR|socket.SO_BROADCAST, 1)
+    sock.bind(('', PORT))
+
+    ut = "<?xml version=\"1.0\" encode=\"utf-8\"?><UECS ver=\"1.00-E10s\"><MEMO>{0}</MEMO>"\
+        "<URI>{1}</URI><TIME>{2}</TIME><AT>{3}</AT><IP>{4}</IP></UECS>".format(m,u,t,a,HOST)
+    print(ut)
+    print(PORT)
+    print(ADDRESS)
+    sock.sendto(ut.encode(),(ADDRESS,PORT))
+    sock.close()
+
 
 if __name__ == "__main__":
-    rx_test()
+    e10s_send("TEST","http://test.com/test","20230901140000","1:1:2")
+#    rx_test()
