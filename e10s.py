bc8ce99c0b9b32d8aa4cff4c13146f6fd352e542
Masafumim Horimoto on smena8 <masafumi.horimoto@gmail.com>
2023-09-01 10:29:41 +0900

    途中

--- e10s.py
+++ e10s.py
@@ -21,10 +21,20 @@ def rx_test():
 
 
 def init():
-    try:
-        HOST = netifaces.ifaddresses('eth0')[netifaces.AF_INET][0]['addr']
-        break
-    except KeyError:
-        
+    for i in netifaces.interfaces():
+        if i=="lo":
+            continue
+        try:
+            HOST = netifaces.ifaddresses(i)[netifaces.AF_INET][0]['addr']
+            break
+        except:
+            print(f"no {i} interface")
+            continue
+    ADDRESS = netifaces.ifaddresses('eth0')[netifaces.AF_INET][0]['broadcast']
+    PORT = 16520
+    with socket.socket(socket.AF_INET, socket.SOCK_DGRAM) as sock:
+        sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+        sock.bind((HOST, PORT))
+
 if __name__ == "__main__":
     rx_test()
